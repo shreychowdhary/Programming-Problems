9511f18f57ea2eea9a90010c803dd54f1b6c9d12
Shrey <shrey.chowdhary1@gmail.com>
2017-03-20 20:14:05 -0400

    Finished Preface

--- USACO Preface/src/preface.java
+++ USACO Preface/src/preface.java
@@ -1,6 +1,10 @@
 import java.io.*;
 import java.util.*;
-
+/*
+ID:shrey.c1
+LANG:JAVA
+TASK:preface
+*/
 public class preface {
 
 	public static void main(String[] args) throws IOException {
@@ -11,21 +15,40 @@ public class preface {
 		char[] numerals = {'I','V','X','L','C','D','M'};
 		int[] numVals = {1,5,10,50,100,500,1000};
 		int[] numCount = new int[numerals.length];
-		int num = n;
-		for(int i = numVals.length-1; i >= 0; i--){
-			if(num/numVals[i] == 0 ){
-				continue;
-			}
-			else{
-				int count = num/numVals[i];
-				System.out.println(count + " " + numVals[i]);
-				num = num%numVals[i];
+		for(int i = 1; i <= n; i++){
+			int num = i;
+			String st = "";
+			for(int j = numVals.length-1; j >= 0; j--){
+				int count = num/numVals[j];
 				
+				
+				//.out.println(count + " " + numVals[i]);
+				num = num%numVals[j];
+				if(count <= 3){
+					for(int k = 0; k < count; k++){
+						st += numerals[j];
+						numCount[j]++;
+					}
+				}
+				else if(count > 3){
+					st += numerals[j] + "" + numerals[j+1];
+					numCount[j]++;
+					numCount[j+1]++;
+				}
+				if(j%2 == 0 && j >= 2 && num/numVals[j-2] == 9){
+					st += numerals[j-2] + "" + numerals[j];
+					num -= numVals[j-2] * 9;	
+					numCount[j-2]++;
+					numCount[j]++;
+				}
 			}
 		}
-		
-		
-		
+		for(int i = 0; i < numCount.length; i++){
+			if(numCount[i] > 0){
+				out.println(numerals[i] + " " + numCount[i]);
+			}
+		}
+		out.close();
 	}
 	
 }
